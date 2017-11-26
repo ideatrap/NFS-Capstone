972c5e2c0bf230f27258539eb56784e19befa366
joezq <zhouqi.coding@gmail.com>
2017-11-26 13:47:32 +0800

    wp2

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -103,7 +103,8 @@ class WaypointUpdater(object):
                     min_dist = dist
             self.next_waypoint_index = wp_ahead_index
         else:#starts from known position
-            for for i, waypoint in enumerate (self.base_waypoints[:(next_waypoint_index+50)]):
+            end_index = min(self.next_waypoint_index+50, self.num_waypoints-1)
+            for i, waypoint in enumerate (self.base_waypoints[self.next_waypoint_index:end_index]):
                 wp_x = waypoint.pose.pose.position.x
                 wp_y = waypoint.pose.pose.position.y
                 dist = self.distance(wp_x,wp_y,pos_x, pos_y)

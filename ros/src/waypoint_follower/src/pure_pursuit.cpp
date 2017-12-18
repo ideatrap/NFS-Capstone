7221e482da0b706de39b362ffd7e43b06a38d4fd
Mike Rzucidlo <mfrstatements@yahoo.com>
2017-12-18 14:43:15 -0500

    Update pure_pursuit.cpp with queue size of 1

--- ros/src/waypoint_follower/src/pure_pursuit.cpp
+++ ros/src/waypoint_follower/src/pure_pursuit.cpp
@@ -31,7 +31,14 @@
 #include "pure_pursuit_core.h"
 
 constexpr int LOOP_RATE = 30; //processing frequency
-
+/** http://wiki.ros.org/rospy/Overview/Publishers%20and%20Subscribers 
+  "Setting the queue_size to 1 is a valid approach if you want to make sure
+   that a new published value will always prevent any older not yet sent
+   values to be dropped. This is good for, say, a sensor that only cares
+   about the latest measurement. e.g. never send older measurements if a
+   newer one exists."
+*/
+const int QUEUE_SIZE = 1;
 
 int main(int argc, char **argv)
 {
@@ -56,11 +63,11 @@ int main(int argc, char **argv)
   ROS_INFO("set subscriber...");
   // subscribe topic
   ros::Subscriber waypoint_subscriber =
-      nh.subscribe("final_waypoints", 10, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
+      nh.subscribe("final_waypoints", QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
   ros::Subscriber ndt_subscriber =
-      nh.subscribe("current_pose", 10, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
+      nh.subscribe("current_pose", QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
   ros::Subscriber est_twist_subscriber =
-      nh.subscribe("current_velocity", 10, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
+      nh.subscribe("current_velocity", QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
 
   ROS_INFO("pure pursuit start");
   ros::Rate loop_rate(LOOP_RATE);

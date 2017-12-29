873688b33fa753f64889ea85c0202c5cd12f9996
joezq <zhouqi.coding@gmail.com>
2017-12-29 21:51:34 +0800

    updated server

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -9,9 +9,9 @@ from flask import Flask, render_template
 from bridge import Bridge
 from conf import conf
 
-eventlet.monkey_patch()
+#eventlet.monkey_patch()
 
-sio = socketio.Server(async_mode='eventlet')
+#sio = socketio.Server(async_mode='eventlet')
 app = Flask(__name__)
 msgs = []
 
